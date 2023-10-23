972de3b439ec79778569644eb9145db37f929649
Devon Estes <devon@remote.com>
2023-10-23 09:31:17 +0200

    add gitignore

--- lib/deck_manager.exs
+++ lib/deck_manager.exs
@@ -12,9 +12,8 @@ defmodule DeckManager do
     "shrines",
     "slug",
     "doctor_volo",
-    "x_spells"
-    #"chatterfang",
-    #"thalrog"
+    "x_spells",
+    "chatterfang"
   ]
 
   def run() do
